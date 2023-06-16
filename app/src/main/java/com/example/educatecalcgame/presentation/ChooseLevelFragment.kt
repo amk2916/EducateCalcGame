2c3ab43c3f50a99ce64c421367f823973b089bbf
Alexey Kozyrev <tcsid4@gmail.com>
2023-06-16 21:25:05 +0300

    Подключили ViewBinding

--- app/src/main/java/com/example/educatecalcgame/presentation/ChooseLevelFragment.kt
+++ app/src/main/java/com/example/educatecalcgame/presentation/ChooseLevelFragment.kt
@@ -6,13 +6,19 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.educatecalcgame.R
+import com.example.educatecalcgame.databinding.ChooseFragmentLevelBinding
+import com.example.educatecalcgame.databinding.FragmentWelcomeBinding
 
 class ChooseLevelFragment : Fragment() {
+    private var _binding: ChooseFragmentLevelBinding? = null
+    private val binding: ChooseFragmentLevelBinding
+        get() = _binding ?: throw RuntimeException("ChooseLevelFragment is null")
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.choose_fragment_level, container, false)
+    ): View {
+        _binding = ChooseFragmentLevelBinding.inflate(inflater, container,false)
+        return binding.root
     }
 }
--- app/src/main/java/com/example/educatecalcgame/presentation/GameFragment.kt
+++ app/src/main/java/com/example/educatecalcgame/presentation/GameFragment.kt
@@ -6,13 +6,23 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.educatecalcgame.R
+import com.example.educatecalcgame.databinding.FragmentGameBinding
 
 class GameFragment:Fragment() {
+    private var _binding: FragmentGameBinding? = null
+    private val binding: FragmentGameBinding
+        get() = _binding ?: throw RuntimeException("GameFinishedFragment is null")
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_game,container,false)
+    ): View {
+        _binding = FragmentGameBinding.inflate(inflater, container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
     }
 }
